b79d93188322f64c6b7cce1bfe8979aba6fbb37f
WangFushu <wangfushu1992@gmail.com>
2014-05-21 10:16:41 +0800

    完成indexActivity的启动，并设置了启动画面

--- app/src/main/java/com/qdu/jw/app/MainActivity.java
+++ app/src/main/java/com/qdu/jw/app/MainActivity.java
@@ -1,5 +1,7 @@
 package com.qdu.jw.app;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +13,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, IndexActivity.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+            }
+        }, 2000);
     }
 
 
